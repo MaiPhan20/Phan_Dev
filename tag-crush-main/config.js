78c14e6906546f08f5b8b258d7c605f41f170eca
Mai Phán <65001496+MaiPhan20@users.noreply.github.com>
2022-04-18 23:42:46 +0700

    Update config.js

--- tag-crush-main/config.js
+++ tag-crush-main/config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Hee luu cậu!',
-    introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
+    introTitle: 'Hee luu bạn!',
+    introDesc: `Tớ có điều này muốn hỏi bạn, nhớ phải trả lời thật lòng nha`,
     btnIntro: 'oke',
-    title: 'Cậu yêu tớ có phải không 😙',
-    desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu là vợ tớ đó nha :> ',
+    title: 'Bạn yêu tớ có phải không 😙',
+    desc: 'Bạn không trả lời hoặc thoát ra, tức là bạn là vợ tớ đó nha :> ',
     btnYes: 'yêu ơi là iuuu <33',
-    btnNo: 'cậu mơ à :333',
-    question:'Nói cho tới lý do vì sao cậu yêu tớ đii <3',
+    btnNo: 'bạn mơ à :333',
+    question:'Nói cho tới lý do vì sao bạn yêu tớ đii <3',
     btnReply: 'Gửi cho tớ <3',
-    reply: 'Tại vì cậu đẹp trai hihi <33333333',
-    mess: 'Tớ biết mà 🥰. Love you 1505 😘😘',
-    messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.',
+    reply: 'Tại vì bạn đẹp trai hihi <33333333',
+    mess: 'Tớ biết mà 🥰. Love you 309 😘😘',
+    messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.Cả bầu trời này của tớ dành riêng cho bạn',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/maiphan210200/'
+    messLink: 'https://www.facebook.com/hai.nguyenphuc.276'
 }
